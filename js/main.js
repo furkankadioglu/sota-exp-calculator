ff3ad097eedc6aa9744e88b13490c58120b59d0e
Furkan KADIOĞLU <furkan.kadioglu@gmail.com>
2017-01-25 23:31:36 +0300

    Deleted some useless lines

--- js/main.js
+++ js/main.js
@@ -11,23 +11,13 @@ function calculator($scope)
 	$scope.LevelExp = 1212424;
 	$scope.LevelMultifier = 1.1;
 
-	// Temporary Variables
-
-
-	// Results
-	this.levelDifference = this.targetLevel - this.currentLevel;
-	this.Coefficient = Math.pow(this.LevelMultifier, this.levelDifference);
-	this.currentExp = (this.LevelExp / this.Coefficient).toFixed(2);
-	this.currentExpNeed = (this.LevelExp - this.currentExp).toFixed(2);
-	this.currentGoalPercent = ((100* this.currentExp) / this.LevelExp).toFixed(2);
-
 
 	$scope.calculate = function() {
 		console.log("Okay!");
 		this.levelDifference = this.targetLevel - this.currentLevel;
 		this.Coefficient = (Math.pow(this.LevelMultifier, this.levelDifference)).toFixed(2);
-		this.currentExp = (this.LevelExp / this.Coefficient).toFixed(2);
-		this.currentExpNeed = (this.LevelExp - this.currentExp).toFixed(2);
+		this.currentExp = (this.LevelExp / this.Coefficient).toFixed(0);
+		this.currentExpNeed = (this.LevelExp - this.currentExp).toFixed(0);
 		this.currentGoalPercent = ((100* this.currentExp) / this.LevelExp).toFixed(2);
 
 		console.log("Current Level: " + this.currentLevel);
